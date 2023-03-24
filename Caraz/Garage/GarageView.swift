e3c59ba2fbb877ce9c6e3f3d56dedbee74bbae1b
adeline-honore <honore_adeline@yahoo.fr>
2023-03-24 11:42:02 +0100

    mise en place de l onglet Garage

---
+++ Caraz/Garage/GarageView.swift
@@ -0,0 +1,24 @@
+//
+//  GarageView.swift
+//  Caraz
+//
+//  Created by HONORE Adeline on 24/03/2023.
+//
+
+import UIKit
+
+class GarageView: UIView {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var choiceLabel: UILabel!
+    
+    @IBOutlet weak var choiceImage: UIImageView!
+    
+    @IBOutlet weak var actionLabel: UILabel!
+    
+    @IBOutlet weak var choiceSegmentedControl: UISegmentedControl!
+    
+    
+    
+}
--- Caraz/Garage/GarageViewController.swift
+++ Caraz/Garage/GarageViewController.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class GarageViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    private var garageView: GarageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        garageView = view as? GarageView
     }
     
-
     
-    // MARK: - Navigation
+    // MARK: - UserDefaults
+    
+    
+    
+    // MARK: - Choose the car
+    
 
     
 
