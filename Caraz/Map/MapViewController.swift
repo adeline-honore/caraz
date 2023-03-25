a30764b420fb22b2cb456322a23614fe7acd98d8
adeline-honore <honore_adeline@yahoo.fr>
2023-03-25 22:33:00 +0100

    Map : creation

---
+++ Caraz/Map/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  Caraz
+//
+//  Created by HONORE Adeline on 25/03/2023.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
